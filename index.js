b6ae64bca00c7a80ca69f76bb1fcb579c14656c1
Anish, Nepal <anish.nepal@fastems.com>
2020-05-31 20:01:49 +0300

    update response and request node

--- index.js
+++ index.js
@@ -1,58 +1,23 @@
 const http = require('http');
+const HttpServer = require('./server');
 
-var routes = {
-  '/api/posts': function (req, res) {
-    var posts = "1, 2, 3";
-    res.status(200).send(posts);
-  }
-}
+const server = new HttpServer(8080);
 
-//create a server object:
-http.createServer((req, res) => {
-  var url = req.url;
+  server.get('/', (req, res) => {
+    res.status(200).send("test test")
+  });
+
+  server.get('/api/posts', (req, res) => {
+      var posts = "p1, p2, p3";
+      res.status(200).send(posts);
+  });
+
+  server.post('/api/posts', (req, res) => {
+    const post = req.payload
+    res.status(200).send(post)
+  });
   
-  res.status = (statusCode) => {
-    res.writeHeader(statusCode);
-    return res;
-  }
-
-  res.send = (data) => {
-    res.write(data);
-    res.end();
-  }
-
-  var handler = routes[url];
-
-  if (!handler) {
-    res.end(400);
-    return;
-  }
-
-  try {
-    handler(req, res);
-  } catch (err) {
-    res.write("your request is not succeeded.");
-    res.end();
-  }
-
-
-  // if (req.url === '/') {
-  //   res.write("You are in home page.");
-  //   res.end();    
-  // } 
-  // if (req.url === '/edit') {
-  //   renderContactUsHtml(res);
-  // }
-  // else {
-  //   res.writeHeader(200);
-  //   res.end();
-  // }
+  server.listen();
   
-}).listen(8080); //the server object listens on port 8080
 
-function renderContactUsHtml(res) {
-  var html = '<h2>Hello sir</h2>';
-  res.writeHeader(200, {"Content-Type": "text/html"});  
-  res.write(html);  
-  res.end();  
-}
+  
--- server.js
+++ server.js
@@ -1,47 +1,115 @@
-import http from 'http';
-import { IncomingMessage, ServerResponse } from 'http';
+const http = require('http');
+const { IncomingMessage, ServerResponse } = require('http');
 
-export default class HttpServer {
+class Request extends IncomingMessage {
+    constructor(socket) {
+        super(socket);
+        this.params = {};
+        this.payload = {};
+    }
+    setPayload(payload = {}) {
+        this.payload = payload;
+    }
+    setParams(params = {}) {
+        super.setParams();      
+        this.params = params;
+    }
+}
+
+class Response extends ServerResponse {
+    constructor(req) {
+        super(req);
+    }
+    status(code) {
+        this.writeHeader(code);
+        return this;
+    }
+    send(data) {
+        if (data) {
+            this.write(data);
+        }        
+        this.end();
+    }
+}
+
+class HttpServer {
     
   constructor(port = 3000) {
     this.port = port;
-    this.handlers = {};    
+    this.handlers = {
+        get: {},
+        post: {}, 
+        put: {}, 
+        delete: {}
+    };
+    this.createServer();
   }
 
+  // path: string, handler: Function
   get(path, handler) {
-    this.handlers[path] = handler;
+    this.handlers.get[path] = handler
   }
 
   post(path, handler) {
-    this.handlers[path] = handler;
+    this.handlers.post[path] = handler
   }
 
-  delete(path, handler) {}
+  delete(path, handler) {
+      this.handlers.delete[path] = handler
+  }
 
   put(path, handler) {
-    // Your code here
+    this.handlers.put[path] = handler
   }
 
   listen() {
-    http.createServer((req, res) => {
-        console.log("Server listening at port " + this.port);
-        this.handleRequest(req, res);
-    });
+    this._server.listen(this.port, () => {
+        console.log(`server listening on port ${this.port}`)
+    })
   }
 
-  handleRequest(req, res) {
-    var handler = this.handlers[url];
-
-    if (!handler) {
-      res.end(400);
-      return;
+  _handleRequest(req, res) {
+    const method = req.method.toLowerCase();
+    var methodHandler = this.handlers[method];
+    if (!methodHandler) {
+        console.error(`Method ${method} is not handled.`)
+        res.status(501).send();
+        return;
     }
-  
+
+    var routeFunction = methodHandler[req.url];
+    
+    if (!routeFunction) {
+        console.log(`No route handler found for the path ${req.url}`);
+        res.status(400).send();
+        return;
+    } 
+    
     try {
-      handler(req, res);
+      routeFunction(req, res);
     } catch (err) {
-      res.write("your request is not succeeded.");
-      res.end();
+      res.status(500).send("Unexpected error occured");
     }
   }
+
+  createServer() {
+    var options = {
+        "IncomingMessage": Request,
+        "ServerResponse": Response
+    };
+
+    this._server = http.createServer(options, (req, res) => {    
+      let buffer = '';
+      req.on("data", (chunk) => {
+          buffer += chunk;
+      });
+
+      req.on("end", () => {
+          req.payload = buffer;
+          this._handleRequest(req, res);
+      });
+    });
+  }  
 }
+
+module.exports = HttpServer;
